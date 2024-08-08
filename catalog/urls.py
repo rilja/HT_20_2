8b0ba0a93c07f89c476cc5559ae76bc7791d966e
Artemij Rilev <artik200050@gmail.com>
2024-08-08 20:33:52 +0300

    adding navigation in page using buttons

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from catalog.views import home
+from catalog.views import home, product_details
+
+app_name = 'catalog'
 
 urlpatterns = [
-    path('', home)
+    path('', home, name='catalog_home'),
+    path('product/<int:pk>/', product_details, name='product_details')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from catalog.models import Product
 
@@ -7,3 +7,9 @@ def home(request):
     products = Product.objects.all()
     context = {"product_list": products}
     return render(request, 'catalog/product_list.html', context)
+
+
+def product_details(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    context = {"product": product}
+    return render(request, 'catalog/product_details.html', context)
