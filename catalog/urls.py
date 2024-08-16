b74820c2ea291f7b6c20fa8fc51a3d0e4f1e1bc0
Artemij Rilev <artik200050@gmail.com>
2024-08-16 16:41:49 +0300

    FBV to CBV

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 
-from catalog.views import home, product_details
+from catalog.views import ProductListView, ProductDetailView
 
 app_name = 'catalog'
 
 urlpatterns = [
-    path('', home, name='catalog_home'),
-    path('product/<int:pk>/', product_details, name='product_details')
+    path('', ProductListView.as_view(), name='catalog_home'),
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_details')
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,17 @@
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from catalog.models import Product
 
 
-def home(request):
-    products = Product.objects.all()
-    context = {"product_list": products}
-    return render(request, 'catalog/product_list.html', context)
+class ProductListView(ListView):
+    model = Product
 
 
-def product_details(request, pk):
-    product = get_object_or_404(Product, pk=pk)
-    context = {"product": product}
-    return render(request, 'catalog/product_details.html', context)
+class ProductDetailView(DetailView):
+    model = Product
+
+# def product_details(request, pk):
+#     product = get_object_or_404(Product, pk=pk)
+#     context = {"product": product}
+#     return render(request, 'catalog/product_detail.html', context)
