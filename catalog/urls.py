68057ade97776975f60b460a51e66243be67c6e7
Artemij Rilev <artik200050@gmail.com>
2024-09-10 16:40:57 +0300

    added product create view

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from catalog.views import ProductListView, ProductDetailView
+from catalog.views import ProductListView, ProductDetailView, ProductCreateView
 
 app_name = 'catalog'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='catalog_home'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_details')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product_details'),
+    path('create/', ProductCreateView.as_view(), name='create'),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, DeleteView
+from pytils.translit import slugify
 
 from catalog.models import Product
 
@@ -11,7 +13,21 @@ class ProductListView(ListView):
 class ProductDetailView(DetailView):
     model = Product
 
-# def product_details(request, pk):
-#     product = get_object_or_404(Product, pk=pk)
-#     context = {"product": product}
-#     return render(request, 'catalog/product_detail.html', context)
+
+class ProductCreateView(CreateView):
+    model = Product
+    fields = ('name', 'description', 'image', 'cost', 'created_at', 'updated_at', 'category')
+    success_url = reverse_lazy('products:catalog_home')
+
+    def form_valid(self, form):
+        if form.is_valid():
+            new_product = form.save()
+            new_product.slug = slugify(new_product.name)
+            new_product.save()
+
+        return super().form_valid(form)
+
+
+# class ProductDeleteView(DeleteView):
+#     model = Product
+#     success_url = reverse_lazy('product:list')
